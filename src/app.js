1adaee1a6f35eca63f593584705fe997225d634a
Edupe2603 <31553111+Edupe2603@users.noreply.github.com>
2025-12-11 03:17:45 +0000

    Primer commit del generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let quien = ["Mi perro", "Mi abuela", "El cartero", "Un pajaro"]
+  let accion = [" se comio", " se orino", " destruyo", " rompio"]
+  let cosa = [" mi tarea", " mi carro", " mi telefono"]
+  let cuando = [" antes de clases", " mientras dormia", " mientras hacia ejercicio", " durante mi almuerzo", " mientras rezaba"]
+
+  function aleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  let posicionQuien = aleatorio(quien)
+  let posicionAccion = aleatorio(accion)
+  let posicionCosa = aleatorio(cosa)
+  let posicionCuando = aleatorio(cuando)
+
+  let excusa = posicionQuien + posicionAccion + posicionCosa + posicionCuando
+
+  document.getElementById("excuse").innerHTML = excusa
 };
